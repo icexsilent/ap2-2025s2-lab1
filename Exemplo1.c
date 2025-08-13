f72777602591396b3d933e3d2839a4adf52023c4
Vinicius Rampazi <79593509+icexsilent@users.noreply.github.com>
2025-08-13 01:25:05 +0000

    Ano FCI

--- Exemplo1.c
+++ Exemplo1.c
@@ -4,6 +4,7 @@ int main() {
 	printf("Makenzie - FCI\n");
 	printf("Ciencia da computação ");
 	printf("Alg. e Prog. II\n");
+	printf("A Faculdade de Computação e Informática (FCI) do Mackenzie foi fundada em 1970");
 	
 	//System("pause");
 	return 0;
